a61d6d8477da24d9181865075a33666f5d8f42cd
arat <aratislamsafol@gmail.com>
2022-03-30 00:24:49 +0600

    Project Install

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+// use App\Models\District;
+// use App\Models\Division;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+     /**
+     * @override
+     * Show registrationform override
+     * Compact Distric & Divison for Registration Page
+     *
+     *
+     */
+
+    // public function showRegistrationForm()
+    // {
+    //     $districts = District::orderBy('district_name', 'asc')->get();
+    //     $divisions = Division::orderBy('prioroty', 'asc')->get();
+    //     return view('auth.register', compact('districts', 'divisions'));
+    // }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'f_name' => $data['f_name'],
+            'l_name' => $data['l_name'],
+            'user_name' => $data['l_name'].rand(1, 3000),
+            'age' => $data['age'],
+            'phone' => $data['phone'],
+            'ip_address' => request()->ip(),
+            'status' => 1,
+            'street_address' => $data['street_address'],
+            'division_id' => $data['division_id'],
+            'district_id' => $data['distric_id'],
+            'blood_group' => $data['blood_group'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'remember_token' => Str::random(40),
+        ]);
+    }
+}
